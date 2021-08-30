4bbf65d3819fcf058780431fda79f7f09c7996ac
YashMaurya <yash.maurya537468@gmail.com>
2021-08-31 01:33:13 +0530

    third

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoieWF0emNvb2wiLCJhIjoiY2tzeXo2NHR2MWw3ajMxcHUxYzF6N2p0cCJ9.k5s-EILA3Ouwj2ckR0I08g",
+  },
 };
--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { format } from "date-fns";
 import InfoCard from "../components/InfoCard";
+import Map from "../components/Map";
 
 function Search({ searchResults }) {
   const router = useRouter();
@@ -21,9 +22,11 @@ function Search({ searchResults }) {
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
           <p className="text-xs">
-            300+ Stays - {range} - for {noOfGuests} guests
+            300+ Stays - {range} - {noOfGuests} guests
           </p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays {location}</h1>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
 
           <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
@@ -49,6 +52,10 @@ function Search({ searchResults }) {
               )
             )}
           </div>
+        </section>
+
+        <section className="hidden xl:inline-flex xl:min-w-[600px]">
+          <Map searchResults={searchResults} />
         </section>
       </main>
 
